2f2489ca1c1fd6231d0820ffa57fbb1d2bfed9e0
Arnold Taremwa <taremwaarnold@gmail.com>
2024-10-23 17:28:43 +0100

    Update tsserver to ts_ls

--- init.lua
+++ init.lua
@@ -670,15 +670,15 @@ require('lazy').setup({
         elixirls = {},
         -- gopls = {},
         -- rust_analyzer = {},
-        tsserver = {},
+        ts_ls = {},
         -- html = { filetypes = { 'html', 'twig', 'hbs'} },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        -- ts_ls = {},
         --
 
         lua_ls = {
@@ -721,7 +721,7 @@ require('lazy').setup({
             local server = servers[server_name] or {}
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for tsserver)
+            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end,
