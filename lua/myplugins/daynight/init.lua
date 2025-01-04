5b0f3a488bfeac73bee9868ccf14137ed1f974b2
Arnold Taremwa <taremwaarnold@gmail.com>
2025-01-04 20:17:15 +0000

    Refactor daynight plugin

--- lua/myplugins/daynight/init.lua
+++ lua/myplugins/daynight/init.lua
@@ -1,37 +1,60 @@
--- Change the colorscheme based on time of day
+---@class DayNightConfig
+---@field day_start number Hour to start day mode (0-23)
+---@field day_end number Hour to end day mode (0-23)
+---@field light_theme string Theme to use during day
+---@field dark_theme string Theme to use during night
+---@field check_interval number Interval in ms to check time (default: 300000)
 
-local M = {} -- create the module table
+local M = {}
 
--- Function to switch colorscheme / theme based on the time
-function M.switch_color_scheme()
+---@type DayNightConfig
+M.config = {
+  day_start = 8,
+  day_end = 18,
+  light_theme = 'gruvbox-material',
+  dark_theme = 'tokyonight-storm',
+  check_interval = 300000,
+}
+
+---Update colorscheme based on time of day
+---@private
+function M._update_theme()
   local current_hour = tonumber(os.date '%H')
-  local day_start = 8
-  local day_end = 18
+  local is_daytime = current_hour >= M.config.day_start and current_hour < M.config.day_end
 
-  if current_hour >= day_start and current_hour < day_end then
-    vim.opt.background = 'light'
-  else
-    vim.opt.background = 'dark'
-  end
+  -- Set background and theme based on time
+  vim.opt.background = is_daytime and 'light' or 'dark'
+  local theme = is_daytime and M.config.light_theme or M.config.dark_theme
 
-  -- Set tokyonight theme
-  vim.cmd [[colorscheme gruvbox-material]]
-
-  require('tokyonight').setup { style = 'storm' }
+  pcall(vim.cmd.colorscheme, theme)
 end
 
--- Function to initialize the module
-function M.init()
-  M.switch_color_scheme() -- switch the colorscheme on startup
+---Setup the daynight plugin
+---@param opts? DayNightConfig
+function M.setup(opts)
+  -- Merge user config with defaults
+  M.config = vim.tbl_deep_extend('force', M.config, opts or {})
 
-  -- Set a timer to switch the colorscheme every hour
-  vim.loop.new_timer():start(
-    0,
-    300000,
-    vim.schedule_wrap(function()
-      M.switch_color_scheme()
-    end)
-  )
+  -- Initial theme update
+  M._update_theme()
+
+  -- Create autogroup for the plugin
+  local group = vim.api.nvim_create_augroup('DayNight', { clear = true })
+
+  -- Setup timer for theme updates
+  vim.api.nvim_create_autocmd('VimEnter', {
+    group = group,
+    callback = function()
+      local timer = vim.loop.new_timer()
+      timer:start(
+        0,
+        M.config.check_interval,
+        vim.schedule_wrap(function()
+          M._update_theme()
+        end)
+      )
+    end,
+  })
 end
 
-return M -- return the module
+return M
