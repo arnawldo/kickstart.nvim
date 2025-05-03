67f9acb171b098930bc9625754d5dc9f69a20915
Arnold Taremwa <taremwaarnold@gmail.com>
2025-05-03 02:15:19 +0100

    refactor: use recommended way of configuring tmux-navigator

--- lua/custom/plugins/tmux_navigator.lua
+++ lua/custom/plugins/tmux_navigator.lua
@@ -1,15 +1,16 @@
 return {
-  -- Seamlessly move between tmux panes and vim splits
   'christoomey/vim-tmux-navigator',
-  config = function()
-    -- Disable the default mappings
-    vim.g.tmux_navigator_no_mappings = 1
-
-    -- Define custom mappings
-    vim.keymap.set('n', '<C-h>', '<cmd>TmuxNavigateLeft<CR>', { silent = true, desc = 'Navigate to left pane' })
-    vim.keymap.set('n', '<C-j>', '<cmd>TmuxNavigateDown<CR>', { silent = true, desc = 'Navigate to down pane' })
-    vim.keymap.set('n', '<C-k>', '<cmd>TmuxNavigateUp<CR>', { silent = true, desc = 'Navigate to up pane' })
-    vim.keymap.set('n', '<C-l>', '<cmd>TmuxNavigateRight<CR>', { silent = true, desc = 'Navigate to right pane' })
-    -- vim.keymap.set('n', '<C-\\>', '<cmd>TmuxNavigatePrevious<CR>', { silent = true, desc = 'Navigate to previous pane' })
-  end,
+  cmd = {
+    'TmuxNavigateLeft',
+    'TmuxNavigateDown',
+    'TmuxNavigateUp',
+    'TmuxNavigateRight',
+    'TmuxNavigatorProcessList',
+  },
+  keys = {
+    { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+    { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+    { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+    { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+  },
 }
