52a6506feaa3b2f3dff856c4d367c42605ab9053
Arnold Taremwa <taremwaarnold@gmail.com>
2025-08-10 19:17:11 +0100

    feat: replace todo-comments plugin with mini.comments

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -34,6 +34,9 @@ return { -- Collection of various small independent plugins/modules
     -- Animations
     require('mini.animate').setup()
 
+    -- Comments
+    require('mini.comment').setup()
+
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
   end,
