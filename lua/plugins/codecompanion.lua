864b1fc7e8b4273d5ca7f576fb3c8e20f4427af8
Arnold Taremwa <taremwaarnold@gmail.com>
2025-10-17 05:12:58 +0100

    feat: add codecompanion ai plugin

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,22 @@
+-- Expand 'cc' into 'CodeCompanion' in the command line
+vim.cmd [[cab cc CodeCompanion]]
+
+return {
+  'olimorris/codecompanion.nvim',
+  opts = {
+    strategies = {
+      chat = {
+        adapter = 'anthropic',
+        model = 'claude-sonnet-4-20250514',
+      },
+    },
+    -- NOTE: The log_level is in `opts.opts`
+    opts = {
+      log_level = 'DEBUG',
+    },
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'ravitemer/mcphub.nvim',
+  },
+}
